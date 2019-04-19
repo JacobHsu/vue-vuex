c087e4056212b80c76383dadf78fe92c93eb3564
jacob.hsu <jacob.hsu@git4u.net>
2019-04-19 17:33:11 +0800

    mapState

--- try-vuex/README.md
+++ try-vuex/README.md
@@ -2,6 +2,12 @@
 
 安装 `npm install vuex --save`
 
+`store` directory index.js  
+`main.js` import 
+
+[詳解vuex中mapState,mapGetters,mapMutations,mapActions的作用](https://codertw.com/前端開發/203146/)  
+map其實就是一個在store檔案中的對映而已，就是不用讓你要呼叫一個值需要敲這麼多程式碼
+
 ## Project setup
 ```
 npm install
--- try-vuex/src/App.vue
+++ try-vuex/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="increment">加{{count}}</button> 
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {mapState,mapActions} from 'vuex' 
 
 export default {
   name: 'app',
+  data () { 
+    return { 
+      msg: 'Welcome to Your Vue.js App' 
+    } 
+  }, 
+  methods:{ 
+    increment() { 
+      this.$store.dispatch('incrementsync').then(() => { 
+        console.log('then'); 
+      }); 
+    } 
+  }, 
+  computed:
+    mapState({ // mapState相當於對映 
+      count: 'numb', //這邊的意思是mapState把'numb'的值對映給了count，所以count等於10086 
+    }),
   components: {
     HelloWorld
   }
